f01c7e286c75255f22d2c3ac671662a4768a43a4
Thorben0 <thorben.stumpe@gmail.com>
2022-01-25 16:44:33 +0100

    :loud_sound: Log to file

---
+++ src/main/java/com/shopicorn/gateway/LoggingFilter.java
@@ -0,0 +1,31 @@
+package com.shopicorn.gateway;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class LoggingFilter implements GlobalFilter {
+	Log log = LogFactory.getLog(getClass());
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+		String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+		URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+		log.info("REQUEST: " + originalUri + " -> " + routeUri);
+		return chain.filter(exchange);
+	}
+}
