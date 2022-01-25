d6fb3bafe2d5414272ea9d6f271114fd5b66adde
Thorben0 <thorben.stumpe@gmail.com>
2022-01-25 16:22:07 +0100

    :sparkles: Add calculator & product routes

--- src/main/java/com/shopicorn/gateway/GatewayServiceApplication.java
+++ src/main/java/com/shopicorn/gateway/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.shopicorn.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,4 +12,13 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("calculator", r -> r.path("/calculator/mwst/**").uri("http://localhost:3003"))
+				.route("products", r -> r.path("/product/**").uri("http://localhost:3001"))
+				.build();
+	}
+
 }
